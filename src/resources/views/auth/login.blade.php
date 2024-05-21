0ad50c4a35ca405312741acadcb1ab6be3d4d67c
shikanothuno <ri0066xr@gmail.com>
2024-05-21 14:44:40 +0900

    create lagin and register layout

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,47 +1,27 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('layouts.layout')
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+@section('title')
+    ログイン
+@endsection
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+@section('css')
+    <link rel="stylesheet" href="{{ asset("css/login.css") }}">
+@endsection
+@section('content')
+    <body id="body">
+        <main id="main">
+            <div class="parent">
+                <form action="{{ route("login") }}" method="POST">
+                    @csrf
+                    <h2>ログイン</h2>
+                    <input class="child__input" type="text" name="email" placeholder="メールアドレス">
+                    <input class="child__input" type="text" name="password" placeholder="パスワード">
+                    <button class="child__button">会員登録</button>
+                    <p class="child__p">アカウントをお持ちでない方はこちらから</p>
+                    <a class="child__a" href="{{ route("register") }}">会員登録</a>
+                </form>
+            </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        </main>
+    </body>
+@endsection
--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -1,52 +1,29 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+@extends('layouts.layout')
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+@section('title')
+    会員登録
+@endsection
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+@section('css')
+    <link rel="stylesheet" href="{{ asset("css/register.css") }}">
+@endsection
+@section('content')
+    <body id="body">
+        <main id="main">
+            <div class="parent">
+                <form action="{{ route("register") }}" method="POST">
+                    @csrf
+                    <h2>会員登録</h2>
+                    <input class="child__input" type="text" name="name" placeholder="名前">
+                    <input class="child__input" type="text" name="email" placeholder="メールアドレス">
+                    <input class="child__input" type="text" name="password" placeholder="パスワード">
+                    <input class="child__input" type="text" name="password_confirmation" placeholder="確認用パスワード">
+                    <button class="child__button">会員登録</button>
+                    <p class="child__p">アカウントをお持ちの方はこちらから</p>
+                    <a class="child__a" href="{{ route("login") }}">ログイン</a>
+                </form>
+            </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        </main>
+    </body>
+@endsection
