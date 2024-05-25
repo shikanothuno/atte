c5b7a810007215050758036d065d91bed9d735a8
shikanothuno <ri0066xr@gmail.com>
2024-05-25 12:22:35 +0900

    fix web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,10 +7,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WorkingTimeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
