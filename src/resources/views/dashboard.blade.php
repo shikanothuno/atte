e4ef1afa911c076e1a78382b40652dc4568201a8
shikanothuno <ri0066xr@gmail.com>
2024-06-13 16:57:43 +0900

    fix dashboard

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -1,17 +1,14 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.layout')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+@section('title')
+    ダッシュボード
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset("css/dashboard.css") }}">
+@endsection
+@section('content')
+    <main id="main">
+
+    </main>
+@endsection
